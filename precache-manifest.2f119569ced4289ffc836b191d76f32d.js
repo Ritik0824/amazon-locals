43c58774cd7204b08f5314d5ced95123ea41c0d8
Ritik Yadav <ritik@Ritiks-MacBook-Air.local>
2023-07-10 00:58:21 +0530

    Updates

---
+++ precache-manifest.2f119569ced4289ffc836b191d76f32d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b2de95184a13b348f61f96155db76b",
+    "url": "/amazon-locals/index.html"
+  },
+  {
+    "revision": "9dd0f407fb386280bf0a",
+    "url": "/amazon-locals/static/css/2.587a2a2e.chunk.css"
+  },
+  {
+    "revision": "8213e82cf8366c2b6e9f",
+    "url": "/amazon-locals/static/css/main.44bc4fbe.chunk.css"
+  },
+  {
+    "revision": "9dd0f407fb386280bf0a",
+    "url": "/amazon-locals/static/js/2.e7101dc9.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/amazon-locals/static/js/2.e7101dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8213e82cf8366c2b6e9f",
+    "url": "/amazon-locals/static/js/main.7df8fe47.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/amazon-locals/static/js/main.7df8fe47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b992be4bcfc25df6a4a3",
+    "url": "/amazon-locals/static/js/runtime-main.57b1aa1c.js"
+  }
+]);
